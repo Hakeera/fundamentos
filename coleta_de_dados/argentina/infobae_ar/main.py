b4753f5e202b103cc14efed551881b609a1abd73
Leonardo Petrilli <jxhcbhudgfv@gmail.com>
2023-10-24 11:13:21 -0300

    Estudo web scraping infobae

--- coleta_de_dados/argentina/infobae_ar/main.py
+++ coleta_de_dados/argentina/infobae_ar/main.py
@@ -12,23 +12,38 @@ def acessar_pagina(url):
     return soup 
 
 
-def extrair_infos(url):
-    
-    div = acessar_pagina(url)
-
-
-#Tentando extrair os links e os metadados das notícias através de um laço while com find_next.
- 
+#Tentando extrair os links e os metadados das notícias através de um laço while com find_next
 # Tentativa 1:
- 
-    '''
-    while link != None:
-        geral = div.find('div', class_="feed-list-wrapper")
-        link = geral.find('a')
-        link_url = 'https://www.infobae.com' + link['href']
-        print(link_url)
-        link = link.find_next('a')  #--> Tentei usar find_next dentro do while para "pular entre as notícias" até acabarem (none)
-    '''
+
+links_list = []     # Inicializa uma lista vazia para armazenar os links
+titulo_list = []    # Inicializa uma lista vazia para armazenar os títulos
+subtitulo_list = [] # Inicializa uma lista vazia para armazenar os subtitulos
+
+def extrair_infos(url):
+    soup = acessar_pagina(url)
+    
+    div = soup.find('div', class_='feed-list-wrapper')
+    link = div.find('a')
+
+    while link is not None:
+        link_url = 'https://www.infobae.com' + link['href'] # Monta o link
+        links_list.append(link_url)  # Adiciona o link à lista
+        
+        # Extrai o título 
+        titulo = link.find('h2', class_="feed-list-card-headline-lean")
+        titulo_list.append(titulo) # Adiciona o titulo a lista
+        
+        # Extrai o subtitulo
+        subtitulo = link.find('div', class_='deck')
+        subtitulo_list.append(subtitulo) # Adiciona o subtitulo a lista
+        
+        link = link.find_next('a') # Pula para o próximo link
+
+    return links_list, titulo_list, subtitulo_list  # Retorna as listas
+
+# O looop está pegando links do footer que também possuem a tag <a>, achei que isso não ocorreria já que a div selecionada tem como parâmetro a class_= 'feed-list-wrapper'. Quando tentei link = div.find_next para atribuir o novo link apenas de acordo com a div deu erro.
+
+
 
 # Tentativa 2:
 
@@ -76,10 +91,13 @@ def extrair_infos(url):
 '''
 # Tentei usar, inclusive, as funções de forma separada mas estava com dificuldade de utilizar as funções find/find_all em div. Acabei optando por colocar a função acessar_pagina dentro da função extrair_infos, assim a as funções find conseguem puxar a variável div que defini logo em seguida de formar a soup.
 
+
 def main(): 
     url = 'https://www.infobae.com/ultimas-noticias-america/'
     
     extrair_infos(url)
+    
+    print("Links:\n", links_list, "\n \nTítulos:\n", titulo_list, "\n \nSubtitulos:\n", subtitulo_list)
 
 if __name__ == "__main__": 
     main()
