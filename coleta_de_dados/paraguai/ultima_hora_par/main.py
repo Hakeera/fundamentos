562c8d3411578ff3e29add150dcfb81e03be561a
Leonardo Petrilli <jxhcbhudgfv@gmail.com>
2023-11-01 11:41:35 -0300

    Estudo web scraping Labri

--- coleta_de_dados/paraguai/ultima_hora_par/main.py
+++ coleta_de_dados/paraguai/ultima_hora_par/main.py
@@ -7,12 +7,9 @@ import requests
 
 links_list = []
 titulos_list = []
-
-'''
-    A página possui divs diferentes para as primeiras 3 notícias e depois para o resto das demais 
-'''
-
-
+categorias_list = []
+data_list = []
+subtitulo_list = []
 
 ## Funções ##
 
@@ -21,33 +18,53 @@ def acessar_pagina(url):
     soup = BeautifulSoup(page.text, 'html.parser')
     return soup
 
+
 def extrair_infos(url):
     html = acessar_pagina(url)
     conteudo = html.find('div', attrs={'class': 'Page-content'})
-    
-    lista_noticia = conteudo.find_all('PagePromo')
-    print(lista_noticia)
-    
+
+    lista_noticia = conteudo.find_all('div', 'PageList-items-item')
+
     for noticia in lista_noticia:
 
         # Extrai e monta os links
-        #link = noticia.link['href'].strip()
-        #link = f"{link}"
-        #links_list.append(link)
+        link = noticia.a['href'].strip()
+        link = f"{link}"
+        links_list.append(link)
 
-        '''# Extrai titulos
-        titulo_element = noticia.h2
-        titulo = titulo_element.text.strip()
-        titulos_list.append(titulo)'''
-
+        # Extrai titulos
+        a_tag = noticia.find('div', 'PagePromo-title')
+        titulo = a_tag.a.text
+        titulos_list.append(titulo)
+       
+        # Extrai categorias
+        a_tag = noticia.find('div', 'PagePromo-category')
+        categoria = a_tag.a.text
+        categorias_list.append(categoria)
+        
+        # Extrai data
+        a_tag = noticia.find('div', 'PagePromo-date')
+        data = a_tag.text
+        data_list.append(data)
+        
+        # Extrai data
+        try:
+            a_tag = noticia.find('div', 'PagePromo-description')
+            subtitulo = a_tag.text
+            subtitulo_list.append(subtitulo)
+        except:
+            pass
+        
 def main():
     url = 'https://www.ultimahora.com/ultimas-noticias'
 
     extrair_infos(url)
-    
-    #print("Links: \n", links_list)
-    #, "\n \n Titulos: \n", titulos_list)
-   
+
+    print("Links: \n", links_list)
+    print("Titulos: \n", titulos_list)
+    print("Categoria: \n", categorias_list)
+    print("Data: \n", data_list)
+    print("Subtitulo: \n", subtitulo_list)
 
 if __name__ == "__main__":
     main()
