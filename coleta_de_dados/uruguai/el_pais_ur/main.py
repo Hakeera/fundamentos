874424cc5fafd6eb475120798fe7198d7839cd01
Leonardo Petrilli <jxhcbhudgfv@gmail.com>
2023-11-08 11:04:12 -0300

    Estudo Web Scraping - Labri (Definido script do Jornal El País -Uruguai)

--- coleta_de_dados/uruguai/el_pais_ur/main.py
+++ coleta_de_dados/uruguai/el_pais_ur/main.py
@@ -1,15 +1,52 @@
 # Importar bibliotecas
 from bs4 import BeautifulSoup
 import requests
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
 
 # Criando listas vazias para armazenar os dados
 
 links_list = []
 titulos_list = []
 
+# Categorias do jornal não são bem divididas no html. A página possui categorias genéricas como "mundo/informacion/opinion" mas cada notícias pode ter outras categorias "politica/economia/judiciales/servicios".
+# O jornal possui uma dinâmica de setores diferentes para cada página como o nosso globo rural. Aparentemente uma nova página como se fosse um jornal diferente para cada setor.
+categorias_list = [
+    "mundo",
+    "opinion",
+    "negocios/noticias", # Setor de negocios
+    "informacion"
+]
+
 ## Funções ##
 
+def carregar_mais(url):
+    # Instancia o navegador (Googl Crhome)
+    service = Service()
+    options = webdriver.ChromeOptions()
+
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get(url)
+    
+    # Scroll até o primeiro botão de carregar mais
+    driver.execute_script("window.scrollBy(0, 7000000);")
+    driver.execute_script("window.scrollBy(0, -300);")
+    
+    # Looping que aperta o botão e da scroll até apertar todos
+    while True:
+        try:
+            time.sleep(2)
+            div_botao = driver.find_element(By.CLASS_NAME, 'List-nextPage')
+            botao = div_botao.find_element(By.TAG_NAME, 'a')
+            botao.click()
+            driver.execute_script("window.scrollBy(0, 700000);")
+            time.sleep(2)
+            driver.execute_script("window.scrollBy(0, -300);")
+            
+        except:
+            break
 
 def acessar_pagina(url):
     page = requests.get(url)
@@ -20,7 +57,7 @@ def acessar_pagina(url):
 def extrair_infos(url):
     html = acessar_pagina(url)
     conteudo = html.find('main', attrs={'class': 'Page-oneColumn'})
-    lista_noticia = conteudo.find_all('div', 'PromoBn')
+    lista_noticia = conteudo.find_all('div', 'PromoBasic')
 
 
     for noticia in lista_noticia:
@@ -31,15 +68,18 @@ def extrair_infos(url):
         links_list.append(link)
 
         # Extrai titulos
-        titulo_element = noticia.text
-        titulos_list.append(titulo_element) # O título desse jornal não está contido em um h2, extraindo o texto com o .text conseguimos a data e a hora junto do titulo. Mas não entendi por que ele extrai vários \n em str....
+        titulo = noticia.text
+        titulos_list.append(titulo) # Não entendi por que extrai vários \n em str...
 
 def main():
-    url = 'https://www.elpais.com.uy/ultimas-noticias'
 
-    extrair_infos(url)
-    print("Links: \n", links_list, "\n Titulos: \n", titulos_list)
-
+    for categoria in categorias_list:
+        
+        url = f'https://www.elpais.com.uy/{categoria}'
+        
+        carregar_mais(url)
+        
+        extrair_infos(url)
 
 if __name__ == "__main__":
     main()
