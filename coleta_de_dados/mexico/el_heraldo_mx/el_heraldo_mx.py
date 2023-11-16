7b6233e89c22b5536936271f9eb4f4fb119d210d
Leonardo Petrilli <jxhcbhudgfv@gmail.com>
2023-11-16 13:02:58 -0300

    Estudo webscraping - Labri

--- coleta_de_dados/mexico/el_heraldo_mx/el_heraldo_mx.py
+++ coleta_de_dados/mexico/el_heraldo_mx/el_heraldo_mx.py
@@ -10,7 +10,7 @@ import time
 
 links_list = []
 titulos_list = []
-
+subtitulos_list = []
 
 
 ## Funções ##
@@ -24,14 +24,28 @@ def acessar_pagina(url):
 def extrair_infos(url):
     html = acessar_pagina(url)
     div_noticias = html.find('div', 'col-md-8 col-lg-9')
-    print(div_noticias)
+    artigos = div_noticias.find_all('article')
+    for artigo in artigos:
+        # Links
+        link = artigo.a['href'].strip()
+        links_list.append(link)
+        # Titulos
+        titulo = artigo.a['title'].strip()
+        titulo_rm, titulo_limpo = titulo.split(":", 1)
+        titulo_tratado = titulo_limpo.encode('latin-1').decode('utf-8')
+        titulos_list.append(titulo_tratado)
+        # Subtitulos
+        subtitulo = artigo.find('p').text
+        subtitulo_tratado = subtitulo.encode('latin-1').decode('utf-8')
+        subtitulos_list.append(subtitulo_tratado)
 
 
 def main():
-        
-        url = 'https://heraldodemexico.com.mx/noticias/'
-        
-        extrair_infos(url)
+
+    url = 'https://heraldodemexico.com.mx/mundo/'
+    extrair_infos(url)
+    print(titulos_list, links_list, subtitulos_list)
+   
 
 if __name__ == "__main__":
     main()
